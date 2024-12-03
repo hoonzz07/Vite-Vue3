bc72becd41b2664860ffbe6bb558078ed7721347
hoonzz <wjdwogns0070120@gmail.com>
2024-12-03 14:18:31 +0900

    import apiUrl , Title

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -7,6 +7,11 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const apiUrl = import.meta.env.VITE_API_URL;
+const appTitle = import.meta.env.VITE_APP_TITLE;
+
+console.log('apiUrl : ' , apiUrl);
+console.log('appTitle : ' , appTitle);
 
 app.use(createPinia())
 app.use(router)
